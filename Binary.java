fc91d9509926149006703a5e4278483b2bf17bae
Aditya-Confluxsys <aditya.deshmukh@confluxsys.com>
2024-09-23 12:07:43 +0530

    Added a Section

--- Binary.java
+++ Binary.java
@@ -3,8 +3,11 @@ package Code;
 class Binary{
     public static void main(String[] args) {
         int[] nums = {1,22,33,44,55,66,77,88,99,100,102};
+        int[] nums = {102,99,88,77,66,55,44,33,22,11,10};
         int target = 55;
+        int target = 44;
         System.out.println(Search(nums, target));
+        System.out.println(Search1(nums1, target1));
     }
 
     static int Search (int[] nums, int target){
@@ -16,6 +19,22 @@ class Binary{
                 mid = end - 1;
             } else if(target>nums[mid]){
                 mid = start + 1; 
+            } else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    static int Search1 (int[] nums1, int target1){
+        int start = 0;
+        int end = nums.length-1;
+        while (start<=end) {
+            int mid = start + (end-start) / 2;
+            if(target<nums[mid]){
+                mid = start + 1;
+            } else if(target>nums[mid]){
+                mid = end - 1; 
             } else{
                 return mid;
             }
